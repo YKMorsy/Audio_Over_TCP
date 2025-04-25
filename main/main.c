66f881dee58d91caa50d04a7f978f31783d58404
YKMorsy <YKM3@pitt.edu>
2025-04-24 22:39:37 -0400

    Initial commit

---
+++ main/main.c
@@ -0,0 +1,106 @@
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "rom/ets_sys.h"
+#include "driver/adc.h"
+#include "esp_log.h"
+
+#define TAG_MIC "ADC_MIC_READ"
+#define TAG_TH "TH"
+
+#define TEMP_HUM_DATA_BYTES 5
+#define TEMP_HUM_DATA_BITS (TEMP_HUM_DATA_BYTES*8)
+gpio_num_t TEMP_HUM_PIN = 32;
+
+
+void mic_in(void)
+{
+    // Configure ADC1 Channel 6 (GPIO34)
+    adc1_config_width(ADC_WIDTH_BIT_12);
+    adc1_config_channel_atten(ADC1_CHANNEL_6, ADC_ATTEN_DB_12);
+
+    while (1)
+    {
+        int adc_read = adc1_get_raw(ADC1_CHANNEL_6);
+        ESP_LOGE(TAG_MIC, "ADC Reading (Channel 6 / GPIO34) %d", adc_read);
+        vTaskDelay(pdMS_TO_TICKS(1000)); // delay for 1 sec
+    }
+}
+
+esp_err_t th_await_pin_state(uint32_t timeout, int expected_pin_stae, uint32_t* duration)
+{
+    gpio_set_direction(TEMP_HUM_PIN, GPIO_MODE_INPUT);
+    for (uint32_t i = 0; i < timeout; i += 2)
+    {
+        ets_delay_us(2);
+        if (gpio_get_level(TEMP_HUM_PIN) == expected_pin_stae)
+        {
+            if (duration) *duration = i;
+            return ESP_OK;
+        }
+    }
+
+    return ESP_ERR_TIMEOUT;
+}
+
+esp_err_t fetch_data(uint8_t *data)
+{
+    // pull signal low to initiate read squence w delay 
+    gpio_set_direction(TEMP_HUM_PIN, GPIO_MODE_OUTPUT_OD); // MUST USE PULL-UP RESISTOR
+    gpio_set_level(TEMP_HUM_PIN, 0);
+    ets_delay_us(20000);
+    gpio_set_level(TEMP_HUM_PIN, 1);
+
+    // check if data is ready
+    if (th_await_pin_state(40, 0, NULL) != ESP_OK) ESP_LOGE(TAG_TH,  "Initialization error, phase B");
+    if (th_await_pin_state(88, 1, NULL) != ESP_OK) ESP_LOGE(TAG_TH,  "Initialization error, phase C");
+    if (th_await_pin_state(88, 0, NULL) != ESP_OK) ESP_LOGE(TAG_TH,  "Initialization error, phase D");
+    
+    // read data using durations of low and high
+    uint32_t low_duration;
+    uint32_t  high_duration;
+    for (int i = 0; i < TEMP_HUM_DATA_BITS; i++)
+    {
+        if (th_await_pin_state(65, 1, &low_duration) != ESP_OK) ESP_LOGE(TAG_TH,  "Low bit timeout");
+        if (th_await_pin_state(75, 0, &high_duration) != ESP_OK) ESP_LOGE(TAG_TH,  "High bit timeout");
+        uint8_t b = i/8;
+        uint8_t m = i%8;
+        data[b] |= (high_duration > low_duration) << (7 - m);
+    }
+
+    // check sum
+    if (data[4] != ((data[0] + data[1] + data[2] + data[3]) & 0xFF))
+    {
+        return ESP_ERR_INVALID_CRC;
+    }
+
+    return ESP_OK;
+}
+
+void temp_in(void)
+{
+    float humidity;
+    float temperature;
+
+    // get data buffer
+    uint8_t data[TEMP_HUM_DATA_BYTES] = {}; // 5 bytes of data
+    esp_err_t result = fetch_data(data);
+
+    if (result != ESP_OK)
+    {
+        ESP_LOGE(TAG_TH,  "Fetch data error");
+        return;
+    }
+
+    // get humidity and temperature from data
+    humidity = (float)(data[0] * 10)/10;
+    temperature = (float)(data[2] * 10)/10;
+
+    ESP_LOGI(TAG_TH,  "Sensor data: humidity=%f, temp=%f deg C", humidity, temperature);
+
+}
+
+void app_main(void)
+{
+    // mic_in();
+    temp_in();
+}
